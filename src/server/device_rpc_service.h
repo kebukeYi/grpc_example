8cc968954b53039f5d5f3d1c1febec61a1e62004
kebukeyi <193276660@qq.com>
2026-01-21 17:08:12 +0800

    init

---
+++ src/server/device_rpc_service.h
@@ -0,0 +1,13 @@
+//
+// Created by 19327 on 2026/01/21/星期三.
+//
+#ifndef GRPC_EXPLAME_DEVICE_RPC_SERVICE_H
+#define GRPC_EXPLAME_DEVICE_RPC_SERVICE_H
+
+
+class DeviceRPCServer final : public DerviceService::Service {
+
+};
+
+
+#endif //GRPC_EXPLAME_DEVICE_RPC_SERVICE_H
